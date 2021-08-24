7e29fd5d013a6f2ef9edf98e4419fb174778b872
kichinosukey <kichinosukey@kichinosukey-MacBookPro-525.local>
2021-08-24 15:49:25 +0900

    [add] chapter01

---
+++ chap01/ex1.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from lib import costFunction, gradientDescent, magic, featureNormalize
+
+if __name__ == '__main__':
+
+    # single variable
+    data = np.loadtxt('ex1data1.txt', delimiter=',')
+    X = data[:, 0]
+    y = data[:, 1]
+
+    ax = plt.subplot()
+    ax.scatter(X, y, marker='x', color='red')
+    plt.show()
+
+    X_ = np.array([np.ones(len(X)), X]).T
+    y_ = y.reshape(len(X), 1)
+
+    J = costFunction(X_, y_, np.zeros((2, 1)))
+    print('Testing cost function: %f' % J)
+
+    J = costFunction(X_, y_, np.array([[-1], [2]]))
+    print('Further testing cost function: %f' % J)
+
+    m = len(X)
+    theta = np.zeros((2, 1))
+    iterations = 1500
+    alpha = 0.01
+    theta_min, j_history_01 = gradientDescent(X_, y_, theta, alpha, iterations)
+    print(theta_min)
+
+    J = costFunction(X_, y_, theta_min)
+    print(J)
+
+    ax = plt.subplot()
+    ax.scatter(X, y, marker='x', color='red')
+    ax.plot(X, np.dot(X_, theta_min).reshape(-1), color='blue')
+    plt.show()
+
+    if True: # 3D mesh grid
+        import pandas as pd
+        from lib import gradientDescentHistory
+        h = gradientDescentHistory(X_, y_, theta, alpha, iterations=iterations, intercept=True,debug=False)
+        df = pd.DataFrame({'theta0': h[0], 'theta1': h[1], 'j':h[2]})
+        M, B = np.meshgrid(df['theta0'], df['theta1'])
+        Z = np.meshgrid(df['j'], df['j'])[0]
+        fig = plt.figure(figsize=(20, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(M, B, Z, rstride=1, cstride=1, cmap="plasma")
+        fig.colorbar(surf)
+        plt.savefig('gradient-descend-3d.png')
+
+
+    # # Multi variables
+    # print("<Multi variables>")
+    # data = np.loadtxt('ex1data2.txt', delimiter=',')
+    # X = data[:, 0:2]
+    # y = data[:, 2]
+    # y_ = y.reshape(len(y), 1)
+    # m = len(X)
+
+    # X = np.insert(featureNormalize(X), 0, 1, axis=1)
+
+    # alpha = 0.01
+    # num_iters = 400
+    # theta = np.zeros((3, 1))
+    # theta, j_history_02 = gradientDescent(X, y_, theta, alpha, iterations=num_iters, intercept=True, debug=True)
---
+++ chap01/lib.py
@@ -0,0 +1,134 @@
+import doctest
+doctest.testmod(verbose=True)
+
+import numpy as np
+
+
+def costFunction(X, y, theta):
+    '''
+    >>> X = np.array([[1, 2], [1, 3], [1, 4], [1, 5]])
+    >>> y = np.array([7, 6, 5, 4])
+    >>> theta = np.array([0.1, 0.2])
+    >>> costFunction(X, y, theta)
+    11.945
+
+    >>> X = np.array([[1, 2, 3], [1, 3, 4], [1, 4, 5], [1, 5, 6]])
+    >>> y = np.array([[7], [6], [5], [4]])
+    >>> theta = np.array([[0.1], [0.2], [0.3]])
+    >>> costFunction(X, y, theta)
+    7.017499999999999
+
+    >>> X = np.array([[2, 1, 3], [7, 1, 9], [1, 8, 1], [3, 7, 4]])
+    >>> y = np.array([[2], [5], [5], [6]])
+    >>> theta = np.array([[0.4], [0.6], [0.8]])
+    >>> np.round(costFunction(X, y, theta), 5)
+    5.295
+    '''
+    m = len(X)
+    h = np.dot(X, theta)
+    return 1/(2*m) * np.sum((h - y)**2)
+
+def featureNormalize(X):
+    '''
+    >>> featureNormalize(np.array([[1], [2], [3]]))
+    array([[-1.],
+           [ 0.],
+           [ 1.]])
+    >>> featureNormalize(magic(3))
+    array([[ 1.09544512, -1.46059349,  0.36514837],
+           [-0.73029674,  0.        ,  0.73029674],
+           [-0.36514837,  1.46059349, -1.09544512]])
+    '''
+    return (X - np.mean(X)) / np.std(X, ddof=1)
+
+def gradientDescent(X, y, theta, alpha, iterations=1000, intercept=True, debug=False):
+    '''
+    >>> X = np.array([[1, 5], [1, 2], [1, 4], [1, 5]])
+    >>> y = np.array([[1], [6], [4], [2]])
+    >>> theta = np.array([[0], [0]])
+    >>> alpha = 0.01
+    >>> iterations = 1000
+    >>> theta_min, j_hist = gradientDescent(X, y, theta, alpha, iterations)
+    >>> theta_min
+    array([[ 5.21475495],
+           [-0.57334591]])
+    >>> X = np.array([[1, 5], [1, 2]])
+    >>> y = np.array([[1], [6]])
+    >>> theta = np.array([[0.5], [0.5]])
+    >>> alpha = 0.1
+    >>> iterations = 10
+    >>> theta_min, j_hist = gradientDescent(X, y, theta, alpha, iterations, intercept=True)
+    >>> theta_min
+    array([[1.70986322],
+           [0.19229354]])
+    >>> j_hist
+    [5.8853124999999995, 5.713851953124999, 5.547543844726562, 5.386121367211915, 5.229408872324786, 5.0772597232631576, 4.929538399473597, 4.7861152864668055, 4.646865103463483, 4.5116663759254]
+    >>> X = np.array([[2, 1, 3], [7, 1, 9], [1, 8, 1], [3, 7, 4]])
+    >>> y = np.array([[2], [5], [5], [6]])
+    >>> theta = np.array([[0.1], [-0.2], [0.3]])
+    >>> theta_min, j_hist = gradientDescent(X, y, theta, 0.01, 10, intercept=False)
+    >>> theta_min
+    array([[0.1855552 ],
+           [0.50436048],
+           [0.40137032]])
+    >>> j_hist
+    [3.6325468281249997, 1.7660945058596678, 1.0215168888342592, 0.6410083828332604, 0.41530550708361225, 0.272296292180201, 0.17938440052798005, 0.11847852136163604, 0.07842876888755221, 0.05206494606885677]
+    '''
+    m, n = X.shape
+    j_history = []
+    for i in range(iterations):
+        if intercept:
+          x = X[:, 1:]
+          h = theta[0, :] + np.dot(x, theta[1:, :])
+          theta_zero = theta[0, :] - alpha * (1/m) * np.sum(h-y)
+          # theta_one = theta[1:, :] - alpha * (1/m) * np.sum((h-y) * x)
+          theta_one = theta[1:, :] - alpha * (1/m) * np.dot(x.T, (h-y))
+          # theta = np.array([theta_zero, theta_one])
+          theta = np.insert(theta_one, 0, theta_zero).reshape(-1, 1)
+          if debug:
+            print(i)
+            print(theta_zero)
+            print(theta_one)
+            print(theta)
+
+        else:
+          h = np.dot(X, theta)
+          theta = theta - alpha * (1/m) * np.dot(X.T, (h - y))
+          # theta = theta - alpha * (1/m) * np.sum((h - y)*X) Can you explain why this is uncorrect ?? 
+        j_history.append(costFunction(X, y, theta))
+    return theta, j_history
+
+def gradientDescentHistory(X, y, theta, alpha, iterations=1000, intercept=True, debug=False):
+    m, n = X.shape
+    theta0_history = []
+    theta1_history = []
+    j_history = []
+    for i in range(iterations):
+        if intercept:
+          x = X[:, 1:]
+          h = theta[0, :] + np.dot(x, theta[1:, :])
+          theta_zero = theta[0, :] - alpha * (1/m) * np.sum(h-y)
+          # theta_one = theta[1:, :] - alpha * (1/m) * np.sum((h-y) * x)
+          theta_one = theta[1:, :] - alpha * (1/m) * np.dot(x.T, (h-y))
+          # theta = np.array([theta_zero, theta_one])
+          theta = np.insert(theta_one, 0, theta_zero).reshape(-1, 1)
+          if debug:
+            print(i)
+            print(theta_zero)
+            print(theta_one)
+            print(theta)
+
+        else:
+          h = np.dot(X, theta)
+          theta = theta - alpha * (1/m) * np.dot(X.T, (h - y))
+          # theta = theta - alpha * (1/m) * np.sum((h - y)*X) Can you explain why this is uncorrect ?? 
+        theta0_history.append(float(theta[0]))
+        theta1_history.append(float(theta[1]))
+        j_history.append(costFunction(X, y, theta))
+    return theta0_history, theta1_history, j_history
+
+
+if __name__ == '__main__':
+
+  import doctest
+  doctest.testmod(verbose=True)
