640a288d0cfbd5643e4594f3c62e02b57e887222
kichinosukey <kichinosukey@kichinosukey-MacBookPro-525.local>
2021-10-13 09:47:07 +0900

    [update] chap02

--- chap02/ex2.py
+++ chap02/ex2.py
@@ -4,7 +4,8 @@ sys.path.append('../')
 import numpy as np
 import matplotlib.pyplot as plt
 
-from lib import lrCostFunction
+from lib import lrCostFunction, lrCostFunctionReg, gradientDescent, newtonOptimize
+
 
 if __name__ == '__main__':
 
@@ -43,4 +44,26 @@ if __name__ == '__main__':
     print('Cost at initial theta (zeros): %0.5f' % J)
     print('Expected cost (approx): 0.693')
 
-    from scipy.optimize import minimize, rose, rosen_der
+    # gradient descent
+    alpha = 0.00015
+    iterations = 30000
+    theta = np.array([[0], [0], [0]])
+    theta_min, J = gradientDescent(X_, y_, theta, alpha, iterations, intercept=True, debug=False)
+    J, grad = lrCostFunction(X_, y_, theta_min)
+
+    print('Cost at theta found by gradient descent: %0.5f' % J[0])
+    print('Expected cost (approx): 0.203')
+
+    # newton optimize
+    theta = np.array([0, 0, 0])
+    Xtil = np.c_[np.ones(X.shape[0]), X]
+    print(theta.shape)
+    print(Xtil.shape)
+    theta_min = newtonOptimize(Xtil, y, theta, max_iter=6)
+    J, _ = lrCostFunction(Xtil, y, theta_min)
+      
+    print('Cost at theta found by fminunc: %0.5f' % J)
+    print('Expected cost (approx): 0.203')
+
+    np.testing.assert_allclose(J, 0.203498, rtol=1e-03)
+    np.testing.assert_allclose(theta_min.reshape(-1, 1), np.array([[-25.161334], [0.206232], [0.201472]]), rtol=1e-03)
--- lib.py
+++ lib.py
@@ -2,26 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 def costFunction(X, y, theta):
     m = len(X)
     h = np.dot(X, theta)
     return 1/(2*m) * np.sum((h - y)**2)
-
-def lrCostFunction(X, y, theta):
-    m = X.shape[0]
-    h = sigmoid(np.dot(X, theta))
-    J = 1/m*sum(-y*np.log(h) - (1-y)*(np.log(1-h)))
-    grad = 1/m*(np.dot(X.T,(h - y)))
-    return J, grad
-
-def lrCostFunctionReg(X, y, theta, alpha):
-    m = X.shape[0]
-    h = sigmoid(np.dot(X, theta))
-    theta_reg = np.r_[np.zeros([1, 1]), theta[1:, :]]
-    J = 1/m*sum(-y*np.log(h) - (1-y)*(np.log(1-h))) + alpha/(2*m)*sum(theta_reg**2) 
-    grad = 1/m*(np.dot(X.T,(h - y))) + alpha/m * theta_reg
-    return J, grad
 
 def featureNormalize(X):
     return (X - np.mean(X)) / np.std(X, ddof=1)
@@ -61,6 +45,49 @@ def gradientDescent(X, y, theta, alpha, iterations=1000, intercept=True, history
     else:
       return theta, j
 
+def hessian(Xtil, yhat, tol=1e-10):
+    r = np.clip(yhat * (1 - yhat), tol, np.inf)
+    XR = Xtil.T * r
+    XRX = np.dot(XR, Xtil)
+    return XRX, XR, r
+
+def lrCostFunction(X, y, theta):
+    m = X.shape[0]
+    h = sigmoid(np.dot(X, theta))
+    J = 1/m*sum(-y*np.log(h) - (1-y)*(np.log(1-h)))
+    grad = 1/m*(np.dot(X.T,(h - y)))
+    return J, grad
+
+def lrCostFunctionReg(X, y, theta, alpha):
+    m = X.shape[0]
+    h = sigmoid(np.dot(X, theta))
+    theta_reg = np.r_[np.zeros([1, 1]), theta[1:, :]]
+    J = 1/m*sum(-y*np.log(h) - (1-y)*(np.log(1-h))) + alpha/(2*m)*sum(theta_reg**2) 
+    grad = 1/m*(np.dot(X.T,(h - y))) + alpha/m * theta_reg
+    return J, grad
+
+def newtonOptimize(Xtil, y, theta, iter=0, max_iter=10, tol=1e-10, diff=np.inf, theta_hist=[], J_hist=[], hist=False):
+    while iter < max_iter and diff > tol:
+        yhat = sigmoid(np.dot(Xtil, theta))
+        XRX, XR, r = hessian(Xtil, yhat)
+        theta_prev = theta
+        #TODO update equation as b must be simple to understand.
+        # b1 = theta_prev - np.dot(np.linalg.inv(XRX), Xtil*(yhat - y))
+        b = np.dot(XR, np.dot(Xtil, theta) - 1 / r * (yhat - y))
+        theta = np.linalg.solve(XRX, b)
+        J = lrCostFunction(Xtil, y, theta)[0]
+        if hist:
+            J_hist.append(J)
+            theta_hist.append(theta)
+        diff = abs(theta_prev - theta).mean()
+        iter += 1
+    
+    if hist:
+        return theta_hist, J_hist
+    else:
+        return theta, J
+
+
 def plotCostSurface(theta0_hist, theta1_hist, j_hist):
     
     df = pd.DataFrame({'theta0': theta0_hist, 'theta1': theta1_hist, 'j':j_hist})
@@ -70,35 +97,6 @@ def plotCostSurface(theta0_hist, theta1_hist, j_hist):
     ax = fig.add_subplot(111, projection='3d')
     surf = ax.plot_surface(M, B, Z, rstride=1, cstride=1, cmap="plasma")
     fig.colorbar(surf)
-
-# def gradientDescentHistory(X, y, theta, alpha, iterations=1000, intercept=True, debug=False):
-#     m, n = X.shape
-#     theta0_history = []
-#     theta1_history = []
-#     j_history = []
-#     for i in range(iterations):
-#         if intercept:
-#           x = X[:, 1:]
-#           h = theta[0, :] + np.dot(x, theta[1:, :])
-#           theta_zero = theta[0, :] - alpha * (1/m) * np.sum(h-y)
-#           # theta_one = theta[1:, :] - alpha * (1/m) * np.sum((h-y) * x)
-#           theta_one = theta[1:, :] - alpha * (1/m) * np.dot(x.T, (h-y))
-#           # theta = np.array([theta_zero, theta_one])
-#           theta = np.insert(theta_one, 0, theta_zero).reshape(-1, 1)
-#           if debug:
-#             print(i)
-#             print(theta_zero)
-#             print(theta_one)
-#             print(theta)
-
-#         else:
-#           h = np.dot(X, theta)
-#           theta = theta - alpha * (1/m) * np.dot(X.T, (h - y))
-#           # theta = theta - alpha * (1/m) * np.sum((h - y)*X) Can you explain why this is uncorrect ?? 
-#         theta0_history.append(float(theta[0]))
-#         theta1_history.append(float(theta[1]))
-#         j_history.append(costFunction(X, y, theta))
-#     return theta0_history, theta1_history, j_history
 
 def sigmoid(X):
     z = np.multiply(X, -1)
